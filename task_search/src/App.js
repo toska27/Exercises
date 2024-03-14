a0fcf0a7c2f22fe024720e66638ce00044c1f50a
toska27 <nemanjatoskic58@gmail.com>
2024-03-14 13:03:34 +0100

    search by name

--- task_search/src/App.js
+++ task_search/src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import users from './Users';
-import People from 'People';
+import People from './People';
 
 function App() {
   const [people, setPeople] = useState(users);
+  const [query, setQuery] = useState(''); 
 
   return (
     <div className="app">
-      <input type='text' placeholder='Search...' className='search' />
-      <People people={people} />
+      <input type='text' placeholder='Search...' className='search' onChange={(e) => setQuery(e.target.value)} />
+      <People people={people} query={query}/>
     </div>
   );
 }
--- task_search/src/People.js
+++ task_search/src/People.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Person from 'Person';
+import Person from './Person';
 
-function People({ people }) {
+function People({ people,query }) {
   return (
     <div className="people">  
-        {people.map((user) => {
+        {people.filter(user => user.name.toLowerCase().includes(query)).length > 0 ? 
+          people.filter(user => user.name.toLowerCase().includes(query)).map((user) => {
             return(
                 <Person key={user.id} {...user}/>
             )
-          }) 
+          }) : 
+          <h3>No matches</h3>
         }
     
     </div>
