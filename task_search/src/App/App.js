bb0a467fc93f056ef729c68857d3d2ec00a54bb4
toska27 <nemanjatoskic58@gmail.com>
2024-03-18 13:08:58 +0100

    formatting

--- task_search/src/App/App.js
+++ task_search/src/App/App.js
@@ -1,43 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import People from '../People/People';
-import Loading from '../Loading/Loading';
-import './app.css';
-const url = 'http://localhost:8000/users';
+import React, { useState, useEffect } from "react";
+import People from "../People/People";
+import Loading from "../Loading/Loading";
+import "./app.css";
+const url = "http://localhost:8000/users";
 
 const App = () => {
   const [people, setPeople] = useState(null);
-  const [query, setQuery] = useState(''); 
+  const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(true);
 
   const fetchPeople = async () => {
-    try{
+    try {
       const response = await fetch(url);
       const people = await response.json();
       setLoading(false);
       setPeople(people);
-    } catch (error){
+    } catch (error) {
       setLoading(true);
     }
-  }
+  };
 
   useEffect(() => {
     fetchPeople();
   }, []);
 
-  if(loading){
+  if (loading) {
     return (
       <main>
         <Loading />
       </main>
-    ) 
+    );
   }
 
   return (
     <div className="app">
-      <input type='text' placeholder='Search...' className='search' onChange={(e) => setQuery(e.target.value)} />
-      {people && <People people={people} query={query}/>}
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {people && <People people={people} query={query} />}
     </div>
   );
-}
+};
 
 export default App;
